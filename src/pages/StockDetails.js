6cdb3c79f80a45bdd0dad0a879d9785d7c0e524f
Alphy Gacheru <alphygacheru@gmail.com>
2022-09-12 21:22:12 +0300

    Grab route parameter

--- src/pages/StockDetails.js
+++ src/pages/StockDetails.js
@@ -1,7 +1,15 @@
-const StockDetails = () => (
-  <div>
-    <h3>Stock details</h3>
-  </div>
-);
+import { useParams } from 'react-router-dom';
+
+function StockDetails() {
+  const { date } = useParams();
+  return (
+    <div>
+      <h3>
+        Stock details-
+        {date}
+      </h3>
+    </div>
+  );
+}
 
 export default StockDetails;

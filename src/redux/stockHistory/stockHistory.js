c301cd8ebd38c9a891042af9fc14d128b88fe473
Alphy Gacheru <alphygacheru@gmail.com>
2022-09-14 18:57:17 +0300

    Fix bug to test stock history reducer

--- src/redux/stockHistory/stockHistory.js
+++ src/redux/stockHistory/stockHistory.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 // Actions
-const GET_STOCK_HISTORY = 'aapl-historical-stock-prices/stockHistory/GET_STOCK_HISTORY ';
+const GET_STOCK_HISTORY = 'aapl-historical-stock-prices/stockHistory/GET_STOCK_HISTORY';
 // Api
 const url = 'https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?apikey=5e8de55d316fcc9a05df7bcd5fb087d1';
 
